4aa8fba09757dc9c645efc34adae9cbd0d453548
Jayaraj Poroor <jayaraj.poroor@gmail.com>
2014-02-19 18:19:06 +0530

    fixed licenses in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,17 @@
+; Copyright (c) Jayaraj Poroor. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; GNU Lesser General Public License 3.0 
+; (http://www.gnu.org/copyleft/lesser.html)
+; which can be found in the file agpl-3.0.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
+
 (defproject org.generuse/generuse-lib "0.1.0-SNAPSHOT"
   :description "Generuse libraries"
   :url "http://generuse.org"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU Lesser General Public License 3.0"
+            :url "http://www.gnu.org/copyleft/lesser.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :aot [generuse.lib.exec]
  )
